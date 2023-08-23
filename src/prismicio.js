23125b7e5b736da44a88792a1e70e18eac3344ed
Renaud Bressand <renaudbressand@macbook-pro-de-renaud.home>
2023-08-23 19:02:49 +0200

    update route resolver

--- src/prismicio.js
+++ src/prismicio.js
@@ -17,11 +17,7 @@ export const repositoryName = config.repositoryName;
 // TODO: Update the routes array to match your project's route structure.
 const routes = [
   {
-    type: "homepage",
-    path: "/",
-  },
-  {
-    type: "blog",
+    type: "blog_post",
     path: "/:uid",
   },
 ];
