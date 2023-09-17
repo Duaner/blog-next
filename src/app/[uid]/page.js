21336d003b0db6719c6a1001f32ada1976e5caf8
Renaud Bressand <renaudbressand@macbook-pro-de-renaud.home>
2023-09-17 21:17:44 +0200

    beg fix issue

--- src/app/[uid]/page.js
+++ src/app/[uid]/page.js
@@ -13,11 +13,12 @@ export default async function Page({ params }) {
   const page = await client
     .getByUID("blog_post", params.uid)
     .catch(() => notFound());
+  const textColor = page.data.fontcolor ? page.data.fontcolor : ''
   return (
     <main className={styles.LayoutContainer} style={{color: textColor}}>
       <div id="bg-color" className={styles.bgColor} style={{background : page.data.background_color}}></div>
       <div id="blog-main"  className={styles.blogmain}>
-        <SliceZone slices={page.data.body} components={components} context={{"date":page.first_publication_date, "secondary_color":page.data.secondary_color}} />;
+        <SliceZone slices={page.data.body} components={components} context={{"date":page.first_publication_date, "secondary_color":page.data.secondary_color}} />
       </div>
     </main>
   );
